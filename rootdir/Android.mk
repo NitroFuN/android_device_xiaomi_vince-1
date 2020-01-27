1f66e86390ab15444f844b35185297bcdbb9c8a6
Venkata Rao Kakani <vkakani@codeaurora.org>
2020-01-27 18:33:35 +0530

    post_boot: change ddr freq from fixed to capped level.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -104,7 +104,7 @@ LOCAL_SRC_FILES    := etc/init.gpuboost.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
-<<<<<<< HEAD
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.gpuboost.sh
 LOCAL_MODULE_TAGS  := optional
@@ -161,17 +161,6 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.parallax.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-=======
-# Offline Charging
-include $(CLEAR_VARS)
-LOCAL_MODULE          := chargeonlymode
-LOCAL_MODULE_OWNER    := xiaomi
-LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/chargeonlymode
-LOCAL_MODULE_TAGS     := optional
-LOCAL_MODULE_CLASS    := EXECUTABLES
->>>>>>> 7692dc5... mido :Bring Back memeui Charging Animation
 include $(BUILD_PREBUILT)
 <<<<<<< HEAD
 >>>>>>> fe69e78... mido: XiaomiParts: Add MSM Thermal Control
