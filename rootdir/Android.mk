dd40d3392eadbc876b18863fcd71eda86036dea7
gamehackerever <gamehackerever@gmail.com>
2020-06-25 09:58:27 +0000

    Fix Status Bar Padding

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,14 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 # Device Script
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.vince.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.vince.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -32,32 +24,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.goodix.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
-<<<<<<< HEAD
-=======
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := init.target.rc
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := etc/init.target.rc
-LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := init.oneplus3.rc
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := etc/init.oneplus3.rc
-LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.safailnet.rc
@@ -66,4 +32,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
->>>>>>> 9a106c56... op3: Add safetynet hack
+
